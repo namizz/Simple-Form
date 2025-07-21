51fd5c03be7e295a38379ae7e2ea05306e0e99bf
namizz <139370392+namizz@users.noreply.github.com>
2025-07-21 15:45:40 +0300

    simple form

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ContactForm from "./component/ContactForm";
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/ContactForm.tsx
@@ -0,0 +1,60 @@
+import { DevTool } from "@hookform/devtools";
+import { useForm } from "react-hook-form";
+interface Field {
+  name: string;
+  email: string;
+  msg: string;
+}
+const ContactForm = () => {
+  const form = useForm<Field>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = form;
+
+  const onSubmit = (data: Field) => {
+    console.log(data);
+  };
+
+  return (
+    <form className="contact-form" noValidate onSubmit={handleSubmit(onSubmit)}>
+      <h1>Contact Form</h1>
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          {...register("name", { required: "Name is required" })}
+        />
+        <p className="error">{errors?.name?.message}</p>
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
+        <p className="error">{errors?.email?.message}</p>
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          {...register("msg", { required: "Message is required" })}
+        ></textarea>
+        <p className="error">{errors?.msg?.message}</p>
+      </div>
+      <DevTool control={control} />
+      <button type="submit">Send Message</button>
+    </form>
+  );
+};
+
+export default ContactForm;
